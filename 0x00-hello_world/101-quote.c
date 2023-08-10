08db336db8afa3a707a33a7b178108fe0cef6536
justxd22 <xdjust18@gmail.com>
2023-08-10 17:16:56 +0200

    d

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+
+	const char *message =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);
+}
