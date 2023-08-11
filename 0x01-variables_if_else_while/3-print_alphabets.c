4018eb4256525edd0f23b3f7e294793aa0fd26dd
justxd22 <xdjust18@gmail.com>
2023-08-11 16:52:17 +0200

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function to - print alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (int i = 0; i < 52; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function to - print alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (i == 16 | i == 4)
+		{
+			continue;
+		}
+		putchar(alphabet[i]);
+	}
+	putchar("\n");
+	return (0);
+}
