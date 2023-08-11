1de457701ac7d6f16f43b5c824ddf2b9c7654466
xd <xdjust18@gmail.com>
2023-08-11 23:48:43 +0300

    final

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function to - print alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 26; i > - 1; i--)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function to - print alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdef";
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0'+i);
+	}
+
+	for (i = 0; i < 7; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
