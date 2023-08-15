f92cc6a970aa9187058e3bc7a9066d971762b7da
justxd22 <xdjust18@gmail.com>
2023-08-15 20:06:22 +0200

    g

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,10 @@
 /**
  * _islower - Entry point
  * Function to - print alphabets
+ * @c: The character to be checked.
  * Return: 0/1
  */
 int _islower(int c)
 {
-	return islower(c);
+	return (islower(c));
 }
