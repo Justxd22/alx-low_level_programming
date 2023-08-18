7201de57e149ba9a5b3be868efb4b5d1cf334d33
justxd22 <xdjust18@gmail.com>
2023-08-18 03:56:09 +0200

    f

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * mul - muliply 2 int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - print all numbers
+ * 
+ * No Return
+**/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
