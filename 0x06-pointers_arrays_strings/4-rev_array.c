ccd892dc635ed2bf1b1456e251a3895c34dc133a
justxd22 <xdjust18@gmail.com>
2023-08-23 18:04:23 +0200

    g

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int x, y;
 
-	for (x = 0; x < n/2; x++)
+	for (x = 0; x < n / 2; x++)
 	{
 		y = a[x];
 		a[x] = a[(n - 1) - x];
 		a[(n - 1) - x] = y;
-		
 	}
 }
