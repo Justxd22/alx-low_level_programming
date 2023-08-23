72450ef659c98541e32cc21a1bf2216fbf3ddc37
justxd22 <xdjust18@gmail.com>
2023-08-23 16:46:42 +0200

    f

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - append two strings
+ * @dest: string to add to
+ * @src: string to copy from
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j, k;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{ /* get null byte index */
+	}
+	for (k = 0; src[k] != '\0'; k++)
+	{
+		dest[j + k] = src[i];
+	}
+	dest[j + k] = '\0';
+	return (dest);
+}
