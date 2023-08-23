caf01f3f5533f557626a08a638a0e53bfe08b964
justxd22 <xdjust18@gmail.com>
2023-08-24 00:02:14 +0200

    v

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,13 +9,13 @@ char *leet(char *ss)
 {
 	char leet[5] = {'4', '3', '0', '7', '1'};
 	char s[5]    = {'A', 'E', 'O', 'T', 'L'};
-	int x,z;
+	int x, z;
 
 	for (x = 0; ss[x] != '\0'; x++)
 	{
 		for (z = 0; z < 5; z++)
 		{
-			if (ss[x] == s[z] || (ss[x]-32) == s[z])
+			if (ss[x] == s[z] || (ss[x] - 32) == s[z])
 			{
 				ss[x] = leet[z];
 			}
