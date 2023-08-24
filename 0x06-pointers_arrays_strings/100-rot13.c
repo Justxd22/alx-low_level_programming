c62b52109e1dac64a0a191576d82ff9dc144e3b5
justxd22 <xdjust18@gmail.com>
2023-08-25 00:55:04 +0200

    f

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string to rot13
+ * @s: string to rot13
+ * Return: result
+ */
+char *rot13(char *s)
+{
+	char rot[52] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char ss[52]  = {'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m'};
+	int x, z;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (z = 0; z < 52; z++)
+		{
+			if (s[x] == rot[z])
+			{
+				s[x] = ss[z];
+			}
+		}
+	}
+	return (s);
+}
