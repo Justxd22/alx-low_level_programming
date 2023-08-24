242eb4439a28160f0573564a8fa9b2b51b50d33c
justxd22 <xdjust18@gmail.com>
2023-08-25 00:41:13 +0200

    f

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,29 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-        	_putchar('0');
-        	return;
-	}
-
 	int is_negative = 0;
 	int reverse = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
-        	is_negative = 1;
-        	n = -n;
-    	}
+		is_negative = 1;
+		n = -n;
+	}
 
 	while (n > 0)
 	{
-        	reverse = reverse * 10 + n % 10;
-        	n /= 10;
+		reverse = reverse * 10 + n % 10;
+		n /= 10;
 	}
 
 	if (is_negative)
-       		_putchar('-');
+		_putchar('-');
 
 	while (reverse > 0)
 	{
