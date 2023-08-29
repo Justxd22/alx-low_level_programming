9e076353334ca50d9a2c839ae08b4c8739f03fb8
justxd22 <xdjust18@gmail.com>
2023-08-29 14:59:05 +0200

    i

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills mem with b for X n times at s
+ * @s: address to be filled
+ * @b: what to fill with
+ * @n: for how much fill
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		*(s + x) = b;
+	}
+	return (s);
+}
