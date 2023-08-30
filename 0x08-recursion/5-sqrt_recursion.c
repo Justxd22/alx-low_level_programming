af1c2c1c78cae4bda83a87aa88f1c40361d86073
justxd22 <xdjust18@gmail.com>
2023-08-31 00:36:50 +0200

    i

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,4 +7,9 @@
  */
 int _sqrt_recursion(int n)
 {
+        if (y < 0)
+                return (-1);
+        if (y == 0)
+                return (1);
+        return (n * _pow_recursion(x, y - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,35 @@
 #include "main.h"
 
 /**
+ * is_prime - check if n is prime
+ * @n: int
+ * @h: int helper
+ * Return: result
+ */
+int is_prime(int n, int h)
+{
+	if (h * h > n)
+		return (1);
+	if (n % h == 0 || n % (h + 2) == 0)
+		return (0);
+
+	return (is_prime(n, h + 6));
+}
+
+
+/**
  * is_prime_number - check if n is prime
  * @n: int is prime?
  * Return: result
  */
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	if (n <= 3)
+		return (0);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+
+	return (is_prime(n, 5));
 }
