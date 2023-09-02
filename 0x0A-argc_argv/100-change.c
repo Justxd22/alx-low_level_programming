435ed89510c93a5a726cb5193996bd546564f4f2
justxd22 <xdjust18@gmail.com>
2023-09-02 02:42:41 +0200

    g

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,13 +12,18 @@ int main(int argc, char *argv[])
 {
 	int x, c, r = 0, cc[5] = {25, 10, 5, 2, 1};
 
-	if (argc < 2 || !isdigit(*argv[1]))
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
 	c = atoi(argv[1]);
+	if (c < 0 || !isdigit(*argv[1]))
+	{
+		printf("0\n");
+		return (1);
+	}
 	for (x = 0; x < 5; x++)
 	{
 		while (c >= cc[x])
