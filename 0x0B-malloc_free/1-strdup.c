c9bd5a5e72cf4a0cd341dd330c549f6a4037efd5
justxd22 <xdjust18@gmail.com>
2023-09-05 19:06:52 +0200

    i

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - alloc new mem + fill it with str
+ * @str: string to fill mem
+ * Return: NULL/pointer
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int x, size;
+
+	if (size == 0)
+		return (NULL);
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		size++;
+	}
+	size++;
+
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < size; x++)
+	{
+		a[x] = str[x];
+	}
+	return (a);
+}
