c7dfbe9d777ce35ccad1483576a9495449c5d6b7
justxd22 <xdjust18@gmail.com>
2023-09-05 19:58:09 +0200

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,11 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int x, y, size = 0, size1 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	for (x = 0; s1[x] != '\0'; x++)
 	{
