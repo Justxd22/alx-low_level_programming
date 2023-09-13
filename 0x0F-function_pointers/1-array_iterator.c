8ca5796eb922dca4737b915074ab377ed088e993
justxd22 <xdjust18@gmail.com>
2023-09-14 00:17:28 +0200

    init

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - executes function given as parameter on each element array.
+ * @array: array
+ * @size: array size
+ * @action: function 
+ * NO Return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+	{
+		(*action)(array[x]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: array size
+ * @cmp: function 
+ * Return: index of first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size <= 0)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if((*cmp)(array[x]))
+			return (x);
+	}
+	return (-1);
+}
