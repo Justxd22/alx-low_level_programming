5db9e73ba7784e66505dfc9f1814a0dcc7f09352
justxd22 <xdjust18@gmail.com>
2023-09-15 02:11:44 +0300

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,18 +15,21 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit (1);
 	}
 
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit (2);
 	}
 
 	for (i = 0; i < num_bytes; i++)
+	{
 		printf("%02x ", ptr[i]);
-
+		if (i != num_bytes - 1)
+			printf(" ");
+	}
 	printf("\n");
 
 	return (0);
