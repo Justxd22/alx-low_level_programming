9ba7befac652f9d2ea2db813eef9883a06bb4a91
justxd22 <xdjust18@gmail.com>
2023-09-26 23:37:18 +0300

    i

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * find_listint_loop - find loop in a linked list
@@ -16,8 +15,18 @@ listint_t *find_listint_loop(listint_t *head)
 		x = x->next;
 		y = y->next->next;
 		if (x == y)
-			return (x);
+			break;
 	}
 
-	return (NULL);
+	if (x != y)
+		return (NULL);
+
+	x = head;
+	while (x != y)
+	{
+		x = x->next;
+		y = y->next;
+	}
+
+	return (x);
 }
