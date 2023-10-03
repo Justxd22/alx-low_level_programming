5508bd7d0d26b4858956aaccbfb3f566212b26f4
justxd22 <xdjust18@gmail.com>
2023-10-03 03:05:13 +0300

    g

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,10 +14,10 @@
 int closeffs(int f1, int f2)
 {
 	if (close(f1) == -1)
-		fprintf(stderr, "Error: Can't close fd %d\n", f1), exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", f1), exit(100);
 
 	if (close(f2) == -1)
-		fprintf(stderr, "Error: Can't close fd %d\n", f2), exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", f2), exit(100);
 	return (0);
 }
 
@@ -35,23 +35,23 @@ int main(int ac, char *av[])
 	char buf[1024];
 
 	if (ac <= 2)
-		fprintf(stderr, "Usage: cp file_from file_to"), exit(97);
+		dprintf(2, "Usage: cp file_from file_to"), exit(97);
 
 	ff = open(av[1], O_RDONLY);
 	if (ff == -1)
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	ft = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 | S_IRGRP | S_IWGRP | S_IROTH);
 	if (ft == -1)
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 
 
 	while ((bb = read(ff, buf, 1024)) > 0)
 	{
 		bbw = write(ft, buf, bb);
 		if (bbw == -1)
-			fprintf(stderr, "Error: Can't write to %s\n", av[2]),
+			dprintf(2, "Error: Can't write to %s\n", av[2]),
 closeffs(ff, ft), exit(99);
 	}
 
