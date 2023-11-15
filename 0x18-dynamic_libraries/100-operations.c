0e948444895a8a7c40cddc083f1c15a3b1422921
justxd22 <xdjust18@gmail.com>
2023-11-15 00:35:16 -0800

    #@

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,70 @@
+#include "100-operations.h"
+
+/**
+ * mul - mul
+ *
+ * @a: int1
+ * @b: int2
+ * Return: mul
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * add - add
+ *
+ * @a: int1
+ * @b: int2
+ *
+ * Return: addition
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub
+ *
+ * @a: int1
+ * @b: int2
+ *
+ * Return: diff
+ *
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - div
+ *
+ * @a: int1
+ * @b: int2
+ *
+ * Return: div
+ *
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod
+ *
+ * @a: int1
+ * @b: int2
+ *
+ * Return: mod
+ *
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
