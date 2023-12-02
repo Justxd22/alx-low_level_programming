92d37797179a8ceb064b6db55fb3520dbfe7069f
Justxd22 <xdjust18@gmail.com>
2023-12-02 20:15:21 +0200

    ii

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *check;
 
+	if (!strlen(key) || !ht || !ht->size)
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	check = ht->array[index];
 
