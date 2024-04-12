01d9a5fbaec55c6592f580764db2509bbff47dea
Justxd22 <xdjust18@gmail.com>
2024-04-12 20:06:58 +0200

    iiii

--- 0x1E-search_algorithms/0main.c
+++ 0x1E-search_algorithms/0main.c
@@ -10,12 +10,12 @@
 int main(void)
 {
     int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+        0, 1, 2, 5, 5, 6, 6, 7, 8, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
+    printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
+    printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
     return (EXIT_SUCCESS);
 }
