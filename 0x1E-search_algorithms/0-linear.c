313120387ce06c50981faeed8c042ca4d992b295
Justxd22 <xdjust18@gmail.com>
2024-04-12 19:36:27 +0200

    fix betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,13 +11,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t x;
+	size_t x;
 
-    for (x = 0; x < size; x++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", x, array[x]);
-        if (array[x] == value)
-            return (x);
-    }
-    return (-1);
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
 }
